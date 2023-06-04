46de09a61d6a5a42dc7bfea5304138e74b403088
parker <parkercn@qq.com>
2023-06-04 22:18:30 +0000

    init app

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+
+import {routes} from './config/routes';
+import { createApp } from 'vue'
+import { App } from './App'
+import {createRouter, createWebHashHistory} from 'vue-router'
+import { history } from './shared/history';
+
+
+const router = createRouter({history,routes,})
+
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
