997b53ddc890bb868db600204eec610960365eb6
parker <parkercn@qq.com>
2023-06-05 15:14:18 +0000

    update default route

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,19 +1,17 @@
-import { RouteRecordRaw } from 'vue-router'
-import { First } from '../components/welcome/First'
-import { Fourth } from '../components/welcome/Fourth'
-import { Second } from '../components/welcome/Second'
-import { Third } from '../components/welcome/Third'
-import { Bar } from '../views/Bar'
-import { Foo } from '../views/Foo'
-import { Welcome } from '../views/Welcome'
+import { RouteRecordRaw } from 'vue-router';
+import { First } from '../components/welcome/First';
+import { Fourth } from '../components/welcome/Fourth';
+import { Second } from '../components/welcome/Second';
+import { Third } from '../components/welcome/Third';
+import { Welcome } from '../views/Welcome';
 
 export const routes: RouteRecordRaw[]= [
-  { path: '/', component: Foo },
-  { path: '/about', component: Bar },
+  { path: '/', redirect: '/welcome' },
   { 
     path: '/welcome', 
     component: Welcome ,
     children:[
+        {path: '', redirect: '/welcome/1', },
         {path: '1', component: First,},
         {path: '2', component: Second,},
         {path: '3', component: Third,},
