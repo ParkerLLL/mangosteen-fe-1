0ea19e4b9cc6757992ee7fb78236724abbbf4c34
parker <parkercn@qq.com>
2023-06-06 11:09:10 +0000

    四个欢迎页面

--- src/components/welcome/Fourth.tsx
+++ src/components/welcome/Fourth.tsx
@@ -1,10 +1,21 @@
 import { defineComponent } from "vue";
-
+import s from './First.module.scss';
+import cloud from '../../assets/icons/cloudData.svg'
+import { RouterLink } from "vue-router";
 export const Fourth = defineComponent({
     setup: (props, context)=> {
         return () => (
-            <div>
-                Fourth
+            <div class={s.wrapper}>
+                <div class={s.card}>
+                    <img class={s.icon} src={cloud} />
+                    <h2>会挣钱<br />还要会省钱</h2>
+                </div>
+                <div class={s.action}>
+                    <RouterLink class= {s.fake} to="/start"> 跳过 </RouterLink>
+                    <RouterLink to="/start"> 完成</RouterLink>
+                    <RouterLink class= {s.fake} to="/start"> 跳过 </RouterLink>
+
+                </div>
             </div>
         )
     }
--- src/components/welcome/Third.tsx
+++ src/components/welcome/Third.tsx
@@ -1,10 +1,21 @@
 import { defineComponent } from "vue";
-
+import s from './First.module.scss';
+import chart from '../../assets/icons/visualization.svg'
+import { RouterLink } from "vue-router";
 export const Third = defineComponent({
     setup: (props, context)=> {
         return () => (
-            <div>
-                Third
+            <div class={s.wrapper}>
+                <div class={s.card}>
+                    <img class={s.icon} src={chart} />
+                    <h2>会挣钱<br />还要会省钱</h2>
+                </div>
+                <div class={s.action}>
+                    <RouterLink class= {s.fake} to="/start"> 跳过 </RouterLink>
+                    <RouterLink to="/welcome/4"> 下一页</RouterLink>
+                    <RouterLink to="/start"> 跳过 </RouterLink>
+
+                </div>
             </div>
         )
     }
