db7a1da2a3f9cb4f165aa584db705ae9bcb835a8
parker <parkercn@qq.com>
2023-06-07 21:56:06 +0000

    添加startpage 和 button组件

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -7,6 +7,7 @@ import { Second } from "../components/welcome/Second";
 import { SecondActions } from "../components/welcome/SecondActions";
 import { Third } from "../components/welcome/Third";
 import { ThirdActions } from "../components/welcome/ThirdActions";
+import { StartPage } from "../views/StartPage";
 import { Welcome } from "../views/Welcome";
 
 export const routes: RouteRecordRaw[] = [
@@ -15,11 +16,12 @@ export const routes: RouteRecordRaw[] = [
     path: '/welcome',
     component: Welcome,
     children: [
-      { path: '', redirect: '/welcome/1', },
-      { path: '1', components: { main: First, footer: FirstActions }, },
-      { path: '2', components: { main: Second, footer: SecondActions }, },
-      { path: '3', components: { main: Third, footer: ThirdActions }, },
-      { path: '4', components: { main: Forth, footer: ForthActions }, },
+      { path: '', redirect: '/welcome/1' },
+      { path: '1', name: "Welcome1", components: { main: First, footer: FirstActions }, },
+      { path: '2', name: "Welcome2", components: { main: Second, footer: SecondActions }, },
+      { path: '3', name: "Welcome3", components: { main: Third, footer: ThirdActions }, },
+      { path: '4', name: "Welcome4", components: { main: Forth, footer: ForthActions }, },
     ]
-  }
+  },
+  {path:'/start', component: StartPage}
 ]
--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,20 +1,36 @@
 import { defineComponent, ref, Transition, VNode, watchEffect } from 'vue';
-import { RouteLocationNormalizedLoaded, RouterView } from 'vue-router';
+import { RouteLocationNormalizedLoaded, RouterView, useRoute, useRouter } from 'vue-router';
 import { useSwipe } from '../hooks/useSwipe';
+import { throttle } from '../shared/throttle';
 import s from './Welcome.module.scss'
+
+const pushMap: Record<string, string> = {
+  'Welcome1': '/welcome/2',
+  'Welcome2': '/welcome/3',
+  'Welcome3': '/welcome/4',
+  'Welcome4': '/start',
+}
 export const Welcome = defineComponent({
   setup: (props, context) => {
-    const main = ref<HTMLElement | null>(null)
-    const { direction, swiping } = useSwipe(main)
+    const main = ref<HTMLElement>()
+    const { direction, swiping } = useSwipe(main, { beforeStart: e => e.preventDefault() })
+    const route = useRoute()
+    const router = useRouter()
+    const replace = throttle(() => {
+      const name = (route.name || 'Welcome1').toString()
+      router.replace(pushMap[name])
+    }, 500)
     watchEffect(() => {
-      console.log(swiping.value, direction.value)
+      if (swiping.value && direction.value === 'left') {
+        replace()
+      }
     })
     return () => <div class={s.wrapper}>
       <header>
         <svg>
-          <use xlinkHref='#eggplant'></use>
+          <use xlinkHref='#mangosteen'></use>
         </svg>
-        <h1>茄子记账</h1>
+        <h1>山竹记账</h1>
       </header>
       <main class={s.main} ref={main}>
         <RouterView name="main">
