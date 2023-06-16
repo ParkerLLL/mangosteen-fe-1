feb852f4c5cde8fd69bb4458c77c5705c9967d9f
parker <parkercn@qq.com>
2023-06-16 20:47:37 +0000

    扩展Tabs组件，支持classPrefix

--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -1,14 +1,39 @@
-import { defineComponent, PropType} from 'vue'; 
+import { defineComponent, PropType, ref } from 'vue';
+import { MainLayout } from '../../layouts/MainLayout';
+import { Icon } from '../../shared/Icon';
+import { Tab, Tabs } from '../../shared/Tabs';
 import s from './ItemList.module.scss';
 export const ItemList = defineComponent({
   props: {
-     name: {
-       type: String as PropType<string>
-     }
+    name: {
+      type: String as PropType<string>
+    }
   },
-  setup: (props,context) => {
-     return () => (
-       <div class={s.wrapper}>hello</div>
+  setup: (props, context) => {
+    const refSelected = ref('本月')
+    return () => (
+      <MainLayout>{
+        {
+          title: () => '山竹记账',
+          icon: () => <Icon name="menu" />,
+          default: () => (
+            <Tabs classPrefix={'customTabs'} v-model:selected={refSelected.value}>
+              <Tab name="本月">
+                list 1
+              </Tab>
+              <Tab name="上月">
+                list 2
+              </Tab>
+              <Tab name="今年">
+                list 3
+              </Tab>
+              <Tab name="自定义时间">
+                list 4
+              </Tab>
+            </Tabs>
+          )
+        }
+      }</MainLayout>
     )
   }
 })
--- src/shared/Tabs.tsx
+++ src/shared/Tabs.tsx
@@ -1,55 +1,58 @@
-import { defineComponent, PropType} from 'vue'; 
+import { defineComponent, PropType } from 'vue';
 import s from './Tabs.module.scss';
-export const  Tabs= defineComponent({
+export const Tabs = defineComponent({
   props: {
-     selected: {
-       type: String as PropType<string>,
-       required: false,
-     },
-     onUpdateSelected:{
-        type: Function as PropType<(name:string) => void>,
-        required: false,
-     }
+    classPrefix: {
+      type: String
+    },
+    selected: {
+      type: String as PropType<string>,
+      required: false,
+    },
+    onUpdateSelected: {
+      type: Function as PropType<(name: string) => void>,
+      required: false,
+    }
   },
-  setup: (props,context) => {
-    
-     return () => {
-            const tabs = context.slots.default?.()
-            if(!tabs) return () => null
-            for(let i=0;i<tabs?.length; i++){
-                if(tabs[i].type !== Tab ){
-                    throw new Error('<Tabs> only accept <Tab> as children')
-                }
-                }
-
-            return <div class={s.tabs}>
-                    <ol class={s.tabs_nav}>
-                        {tabs.map(item => 
-                        <li class={item.props?.name === props.selected ? s.selected : ''}
-                            onClick={()=> context.emit('update:selected',item.props?.name)}
-                        >
-                        {item.props?.name}
-                        </li>)}
-
-                    </ol>
-
-                <div>
-                    {tabs.find(item => item.props?.name === props.selected)}
-                </div>  
-            </div>
-            }
+  setup: (props, context) => {
+    return () => {
+      const tabs = context.slots.default?.()
+      if (!tabs) return () => null
+      for (let i = 0; i < tabs.length; i++) {
+        if (tabs[i].type !== Tab) {
+          throw new Error('<Tabs> only accepts <Tab> as children')
+        }
+      }
+      const cp = props.classPrefix
+      return <div class={[s.tabs, cp + '_tabs']}>
+        <ol class={[s.tabs_nav, cp + '_tabs_nav']}>
+          {tabs.map(item =>
+            <li class={[
+              item.props?.name === props.selected ? [s.selected, cp + '_selected'] : '',
+              cp + '_tabs_nav_item'
+            ]}
+              onClick={() => context.emit('update:selected', item.props?.name)}
+            >
+              {item.props?.name}
+            </li>)}
+        </ol>
+        <div>
+          {tabs.find(item => item.props?.name === props.selected)}
+        </div>
+      </div>
+    }
   }
 })
 
-export const  Tab= defineComponent({
+export const Tab = defineComponent({
   props: {
-     name: {
-       type: String as PropType<string>
-     }
+    name: {
+      type: String as PropType<string>
+    }
   },
-  setup: (props,context) => {
-     return () => (
-       <div>{context.slots.default?.()}</div>
+  setup: (props, context) => {
+    return () => (
+      <div>{context.slots.default?.()}</div>
     )
   }
 })
