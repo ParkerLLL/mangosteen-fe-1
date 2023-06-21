dae20394c625c6908748ca2bb35469bdc1d00486
parker <parkercn@qq.com>
2023-06-21 21:26:38 +0000

    修复新增年份出现溢出的bug

--- src/shared/time.tsx
+++ src/shared/time.tsx
@@ -51,7 +51,11 @@ export class Time {
     let date = new Date(this.date.getTime());
     switch (unit) {
       case 'year':
+        const currentDate = date.getDate();
+        date.setDate(1);
         date.setFullYear(date.getFullYear() + amount);
+        const targetDate = new Date(date.getFullYear(), date.getMonth() + 1, 0, 0, 0, 0).getDate(); 
+        date.setDate(Math.min(currentDate, targetDate));
         break;
       case 'month':
         const d = date.getDate() 
